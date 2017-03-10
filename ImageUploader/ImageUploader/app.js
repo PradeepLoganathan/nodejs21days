b7b1097f0cb2797fe3cc1030341c2d015d484114
Pradeep Loganathan <pradeep@pradeeploganathan.com>
2017-03-10 12:46:46 +0530

    Server creation

--- ImageUploader/ImageUploader/app.js
+++ ImageUploader/ImageUploader/app.js
@@ -1,3 +1,17 @@
-'use strict';
+var http = require("http");
+var os = require("os");
 
-console.log('Hello world');
+http.createServer(startServer).listen(8080);
+
+function startServer(request, response) {
+    var hostName = os.hostname();
+    var freeMem = os.freemem();
+    console.log("In CreateServer")
+    response.writeHead(200, { "content-type": "text/plain" });
+    response.write("Hello world");
+    //response.write(hostName);
+    //response.write(freeMem);
+    console.log(hostName);
+    response.end();
+}
+console.log("Started server...")
