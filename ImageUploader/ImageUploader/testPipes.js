670610fe05864036d6a36755ee543f86b250ad6a
Pradeep Loganathan <pradeep@pradeeploganathan.com>
2017-03-10 13:08:16 +0530

    Streams and pipes

---
+++ ImageUploader/ImageUploader/testPipes.js
@@ -0,0 +1,14 @@
+﻿var request = require("request");
+var fs = require("fs");
+
+var s = request("http://pradeeploganathan.com").pipe(fs.createWriteStream("pradeep.html"));
+
+s.on("data", function (dataChunk)
+{
+    console.log("Writing data >>>>>>" + dataChunk);
+});
+
+s.on("done", function ()
+{
+    console.log(">>>>End of data streamed ")
+});
